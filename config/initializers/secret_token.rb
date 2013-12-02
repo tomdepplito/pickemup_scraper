3a8e815e05f2a66ba78a9daa31d2ae93cb03225a
tomdepplito <tomdepplito@gmail.com>
2013-12-01 16:56:42 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PickemupScraper::Application.config.secret_key_base = '609c052e6ef5fac87dc48ebca2b238133ec34f495ec420d85dbba8463b1be9ad19c8384bfa785d768b3ccaa6395402c0553cf261610238142664c12623caa5e7'
